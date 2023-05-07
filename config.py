48c89218b022cb846a5842e08254efeb6b4fa2ae
eltonlooi <92982202+eltonlooi@users.noreply.github.com>
2023-05-08 07:00:56 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cwcsc58b2nkb.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "eltonlooi-bucket"
+customregion = "us-east-1"
+
